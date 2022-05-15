b0bbd200b6acafb2f56e07e7b37a6fbf0cf2c17a
DivRaw <rdivyanshu96@gmail.com>
2022-05-15 13:45:38 +0530

    Updates

---
+++ precache-manifest.cffd873c257ec0310280fd595b6d1c80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c81b5e01e55ada11cc2",
+    "url": "/robofriends/static/css/main.61c36a90.chunk.css"
+  },
+  {
+    "revision": "8c81b5e01e55ada11cc2",
+    "url": "/robofriends/static/js/main.8c81b5e0.chunk.js"
+  },
+  {
+    "revision": "eed852d13a9913d36a70",
+    "url": "/robofriends/static/css/1.7fa44583.chunk.css"
+  },
+  {
+    "revision": "eed852d13a9913d36a70",
+    "url": "/robofriends/static/js/1.eed852d1.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "034211625466795fd6d3457489a256ff",
+    "url": "/robofriends/index.html"
+  }
+];
